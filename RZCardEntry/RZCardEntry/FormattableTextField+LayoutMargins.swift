3a819c5059f3f73c1fad5dbe1e63114cb8a38b67
Jay Clark <jason.clark@raizlabs.com>
2017-07-14 15:25:25 -0400

    leverage layout margins for adjusting textRect

---
+++ RZCardEntry/RZCardEntry/FormattableTextField+LayoutMargins.swift
@@ -0,0 +1,25 @@
+//
+//  FormattableTextField+LayoutMargins.swift
+//  RZCardEntry
+//
+//  Created by Jason Clark on 7/14/17.
+//  Copyright © 2017 Raizlabs. All rights reserved.
+//
+
+import UIKit
+
+extension FormattableTextField {
+
+    override open func textRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, layoutMargins)
+    }
+
+    override open func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, layoutMargins)
+    }
+
+    override open func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, layoutMargins)
+    }
+    
+}
